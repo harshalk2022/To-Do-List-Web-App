c3bbc4fd7c1694936bf6b557874b98fc7a36841b
Harshal Khandalkar <110604461+harshalk2022@users.noreply.github.com>
2022-11-29 02:42:44 +0530

    button color change

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function update() {
             <th scope="row">${index + 1}</th>
             <td>${element[0]}</td>
             <td>${element[1]}</td> 
-            <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})">Delete</button></td> 
+            <td><button class="btn btn-sm btn-danger" onclick="deleted(${index})">Delete</button></td> 
             </tr>`;
     });
     tableBody.innerHTML = str;
